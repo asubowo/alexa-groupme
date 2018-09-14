c5f1f00da8dbe89d0096ace9304d359a88aa9250
Andrew Subowo <andrew.subowo@gmail.com>
2018-09-13 22:26:05 -0400

    Testing regex fixes

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = "/Alexa[,\\s]+play Despacito/ig";
+      botRegex = /Alexa[,\\s]+play Despacito/ig;
   console.log(request);
-  if(request.text && botRegex.test(request.text)) {
+  if (request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
